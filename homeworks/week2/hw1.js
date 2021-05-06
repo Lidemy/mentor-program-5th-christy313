a5441b5520def6c00673a05130c3e451f0347200
Christy <saumoncitron@gmail.com>
2021-05-06 13:44:58 -0400

    fix join

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,9 @@
 function printStars(n) {
-
+    star = '*'
+    for (var i=1; i<=n; i++) {
+        console.log(star)
+    }
 }
 
 printStars(5)
+
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,21 @@
 function join(arr, concatStr) {
-  
+    if  (arr.length === 0) {
+        return ''
+    } 
+    let ans = arr[0]
+    for (var i=1; i<arr.length; i++) {
+        ans = ans + concatStr + arr[i]
+    }
+    return ans
 }
 
+
 function repeat(str, times) {
-  
+    let ans = ''
+    for (var i=1; i<=times; i++) {
+        ans += str
+    }
+    return ans   
 }
 
 console.log(join(['a'], '!'));
